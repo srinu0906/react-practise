6c8cb5dad3b20afeb7cdc99d5a186c549bfcab8b
srinu0906 <164818954+srinu0906@users.noreply.github.com>
2025-05-28 09:01:21 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Car  from './components/car'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello React</h1>
+    <Car/>
+    <Car/>
+    <h2>AMAZING DUDE!</h2>
+    </>
   );
 }
 
